4645d4b9d0ea216088dc0ceede55b41ec5c370a5
Glumboi <58010838+Glumboi@users.noreply.github.com>
2023-09-08 19:45:43 +0200

    Bug fixes

--- src/ValheimHitmarker/KillMessage .cs
+++ src/ValheimHitmarker/KillMessage .cs
@@ -11,10 +11,12 @@ namespace ValheimHitmarker.MonoBehaviours
     {
         public Texture2D killTexture; // The texture to represent a kill
         private List<KillEntry> killEntries = new List<KillEntry>();
-        private int maxKillCount = 5; // Maximum number of kills to display
+        private int maxKillCount = 10; // Maximum number of kills to display
         private float displayDuration = 4.5f;
         private float fadeDuration = 2.0f;
         private float killSpacing = 40f; // Adjust this for spacing between kill textures
+
+        private GUIStyle textStyle;
 
         private void Start()
         {
@@ -24,6 +26,12 @@ namespace ValheimHitmarker.MonoBehaviours
 
         private void OnGUI()
         {
+            // Initialize the GUI style for the text
+            textStyle = new GUIStyle(GUI.skin.label);
+            textStyle.fontStyle = FontStyle.Bold;
+            textStyle.alignment = TextAnchor.MiddleCenter;
+            textStyle.normal.textColor = Color.white;
+
             float centerY = Screen.height * 0.7f; // Adjust the Y position for a lower placement
 
             List<KillEntry> entriesToRemove = new List<KillEntry>();
@@ -43,6 +51,13 @@ namespace ValheimHitmarker.MonoBehaviours
 
                 // Display each kill texture
                 GUI.DrawTexture(new Rect(startX, centerY, 40, 40), killTexture);
+
+                /*Display a small bold text at the bottom
+                 string labelText = entry.killText; Use the text from the entry
+                 float labelX = startX;
+                 float labelY = centerY + 40;
+
+                GUI.Label(new Rect(labelX, labelY, killSpacing, 20), labelText, textStyle);*/
 
                 // Restore the original GUI color
                 GUI.color = originalColor;
@@ -72,7 +87,7 @@ namespace ValheimHitmarker.MonoBehaviours
             yield return new WaitForSeconds(time);
 
             // Code to execute after the delay
-            ShowKill();
+            ShowKill("[A LONG NAME HERE]"); // Default text
         }
 
         public void DebugShow()
@@ -82,7 +97,7 @@ namespace ValheimHitmarker.MonoBehaviours
             StartCoroutine(ExecuteAfterTime(0.5f));
         }
 
-        public void ShowKill()
+        public void ShowKill(string killText)
         {
             if (killEntries.Count >= maxKillCount)
             {
@@ -90,14 +105,16 @@ namespace ValheimHitmarker.MonoBehaviours
                 killEntries.RemoveAt(0);
             }
 
-            // Add a new kill entry
+            // Add a new kill entry with the specified text
             KillEntry entry = new KillEntry();
+            entry.killText = killText;
             entry.displayStartTime = Time.time;
             killEntries.Add(entry);
         }
 
         private class KillEntry
         {
+            public string killText;
             public float displayStartTime;
         }
     }
--- src/ValheimHitmarker/Patches/CharacterPatches.cs
+++ src/ValheimHitmarker/Patches/CharacterPatches.cs
@@ -6,32 +6,48 @@ namespace ValheimHitmarker.Patches
     [HarmonyPatch(typeof(Character))]
     internal static class CharacterPatches
     {
-        [HarmonyPatch("RPC_Damage", new Type[] { typeof(long), typeof(HitData) })]
+        [HarmonyPatch("ApplyDamage", new Type[] {
+            typeof(HitData),
+            typeof(bool),
+            typeof(bool),
+            typeof(HitData.DamageModifier)})]
+
         [HarmonyPostfix]
-        private static void Postfix_RPC_Damage(Character __instance, long sender, HitData hit)
+        private static void Postfix_ApplyDamage(Character __instance,
+            HitData hit,
+            bool showDamageText,
+            bool triggerEffects,
+            HitData.DamageModifier mod = HitData.DamageModifier.Normal)
         {
-            //Check if the attacking gameobject is the player and if so, display the hitmarker
-            if (hit.GetAttacker() == Player.m_localPlayer ||
-                hit.GetAttacker().GetZDOID().UserID == sender)
+            try
             {
+                if (hit.GetAttacker().GetType() != typeof(Player)) return;
+
 #if DEBUG
-                ValheimHitmarkerPlugin.Log.LogInfo($"Sender ID: {sender}");
+
                 ValheimHitmarkerPlugin.Log.LogInfo($"Attacker ID: {hit.GetAttacker().GetZDOID().UserID}");
 
                 ValheimHitmarkerPlugin.Log.LogInfo("Hit something...");
                 ValheimHitmarkerPlugin.Log.LogInfo($"Target dead? {__instance.IsDead()}");
-                ValheimHitmarkerPlugin.Log.LogInfo($"Target health: {__instance.GetHealth()}");
+                ValheimHitmarkerPlugin.Log.LogInfo($"Target health: {__instance.GetHealth()}\n");
 #endif
 
                 if (__instance.GetHealth() <= 0)//hit.GetTotalDamage())
                 {
                     ValheimHitmarkerPlugin.criticalHitMarker.ShowHitMarker();
-                    ValheimHitmarkerPlugin.killMessage.ShowKill();
-
+#if DEBUG
+                    ValheimHitmarkerPlugin.killMessage.DebugShow();
+#else
+                    ValheimHitmarkerPlugin.killMessage.ShowKill($"[{__instance.GetHoverName()}]");
+#endif
                     return;
                 }
 
                 ValheimHitmarkerPlugin.hitMarker.ShowHitMarker();
+            }
+            catch (NullReferenceException)
+            {
+                return;
             }
         }
     }
